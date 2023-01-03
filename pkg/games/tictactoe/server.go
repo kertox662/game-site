49255aa0896ce595268b5d93c05e98e1e9612d36
Misha Melnyk <as.k.github@melnyk.com>
2023-01-03 11:55:33 -0500

    GAM-15 Add game and server logic implementation

---
+++ pkg/games/tictactoe/server.go
@@ -0,0 +1,79 @@
+package tictactoe
+
+import (
+	"context"
+	"errors"
+
+	"github.com/bufbuild/connect-go"
+	"github.com/kertox662/game-site/proto/games/tictactoe"
+	"github.com/kertox662/game-site/proto/games/tictactoe/tictactoeconnect"
+	"go.melnyk.org/hufid"
+)
+
+var _ tictactoeconnect.TicTacToeServiceHandler = (*Server)(nil)
+
+type Server struct {
+	tictactoeconnect.UnimplementedTicTacToeServiceHandler
+	games map[string]*game
+}
+
+func NewServer() *Server {
+	return &Server{
+		games: make(map[string]*game),
+	}
+}
+
+// CreateGame implements connect interface
+func (s *Server) CreateGame(ctx context.Context,
+	req *connect.Request[tictactoe.CreateGameRequest]) (*connect.Response[tictactoe.CreateGameResponse], error) {
+	msg := req.Msg
+	if msg.BoardSize <= 0 {
+		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("non-positive board size"))
+	} else if msg.ConnectTarget <= 0 {
+		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("non-positive connect target"))
+	} else if msg.MaxPlayers <= 0 {
+		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("non-positive max players"))
+	}
+
+	id := string(hufid.NewUniqID(4))
+	s.games[id] = newGame(
+		int(msg.BoardSize),
+		int(msg.MaxPlayers),
+		int(msg.ConnectTarget),
+	)
+
+	return &connect.Response[tictactoe.CreateGameResponse]{
+		Msg: &tictactoe.CreateGameResponse{
+			GameId: id,
+		},
+	}, nil
+}
+
+// GetGameData implements connect interface
+func (s *Server) GetGameData(ctx context.Context,
+	req *connect.Request[tictactoe.GetGameDataRequest]) (*connect.Response[tictactoe.GetGameDataResponse], error) {
+	msg := req.Msg
+	g, ok := s.games[msg.GameId]
+	if !ok {
+		return nil, connect.NewError(connect.CodeNotFound, nil)
+	}
+	return &connect.Response[tictactoe.GetGameDataResponse]{
+		Msg: g.toProto(),
+	}, nil
+}
+
+// MakeMove implements connect interface
+func (s *Server) MakeMove(ctx context.Context,
+	req *connect.Request[tictactoe.MakeMoveRequest]) (*connect.Response[tictactoe.MakeMoveResponse], error) {
+	msg := req.Msg
+	g, ok := s.games[msg.GameId]
+	if !ok {
+		return nil, connect.NewError(connect.CodeNotFound, nil)
+	}
+
+	if err := g.makeMove(int(msg.Player), int(msg.Move.Row), int(msg.Move.Col)); err != nil {
+		return nil, connect.NewError(connect.CodeFailedPrecondition, err)
+	}
+
+	return &connect.Response[tictactoe.MakeMoveResponse]{}, nil
+}
---
+++ pkg/games/tictactoe/server_test.go
@@ -0,0 +1,15 @@
+package tictactoe
+
+import "testing"
+
+func TestServerCreateGame(t *testing.T) {
+
+}
+
+func TestServerGetGame(t *testing.T) {
+
+}
+
+func TestServerMakeMove(t *testing.T) {
+
+}
