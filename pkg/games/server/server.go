8083481e6cfff972a2afcc22e53c47c6ccc358bc
Misha Melnyk <as.k.github@melnyk.com>
2023-01-04 17:12:13 -0500

    GAM-15 Add ReadHeaderTimeout to server
    ReadHeaderTimeout is set to 5 seconds now to mitigate slow loris attacks, as suggested by golanci-lint

--- pkg/games/server/server.go
+++ pkg/games/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/kertox662/game-site/pkg/games/tictactoe"
 	"github.com/kertox662/game-site/proto/games/tictactoe/tictactoeconnect"
@@ -15,7 +16,8 @@ func NewServer(addr string) *http.Server {
 	mux.Handle(tttPath, tttHandler)
 
 	return &http.Server{
-		Addr:    addr,
-		Handler: mux,
+		Addr:              addr,
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 }
